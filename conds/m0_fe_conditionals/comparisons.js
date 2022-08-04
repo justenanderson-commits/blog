f750b961b263d5e445cb85446ca0174bcaa3cb31
Justen Anderson <justen.anderson@gmail.com>
2022-08-03 21:44:36 -0600

    Complete the comparisons exercises

---
+++ conds/m0_fe_conditionals/comparisons.js
@@ -0,0 +1,109 @@
+// In the exercises below, write your own code where indicated
+// to achieve the desired result.
+
+// One example is already completed. Your task is to complete
+// any remaining prompt.
+
+// Make sure to run the file in your command line using `node <filename>.js`
+
+
+// -------------------
+// PART 1: Comparing variables
+// -------------------
+
+var numberTeachers = 4
+var numberStudents = 20
+var stringTeachers = "4"
+var numberDogs = 0
+
+// EXAMPLE: log the result of the comparison: is numberTeachers greater than numberStudents?
+console.log("Is numberTeachers greater than numberStudents?", numberTeachers > numberStudents);
+// this should log: "Is numberTeachers greater than numberStudents?" false
+
+// YOU DO: log the result of the comparison: is numberTeachers less than numberStudents?
+// this should log: true
+console.log("Is numberTeachers less than numberStudents?", numberTeachers < numberStudents);
+
+// YOU DO: log the result of the comparison: is numberTeachers strictly equal to stringTeachers?
+// this should log: false
+console.log("Is numberTeachers strictly equal to stringTeachers?", numberTeachers === stringTeachers);
+
+// YOU DO: log the result of the comparison: is numberTeachers not equal to numberStudents?
+// this should log: true
+console.log("Is numberTeachers not equal to numberStudents?", numberTeachers !== numberStudents);
+
+// YOU DO: log the result of the comparison: is numberStudents greater than or equal to 20?
+// this should log: true
+console.log("Is numberStudents greater than or equal to 20?", numberStudents >= 20);
+// YOU DO: log the result of the comparison: is numberStudents greater than or equal to 21?
+// this should log: false
+console.log("Is numberStudents greater than or equal to 21?", numberStudents >= 21)
+// YOU DO: log the result of the comparison: is numberStudents less than or equal to 20?
+// this should log: true
+console.log("Is numberStudents less than or equal to 20?", numberStudents <=20);
+// YOU DO: log the result of the comparison: is numberStudents less than or equal to 21?
+// this should log: true
+console.log("Is numberStudents less than or equal to 21?", numberStudents <= 21);
+
+//Got this error: nothing added to commit but untracked files present (use "git add" to track)
+//Tried this: https://careerkarma.com/blog/git-nothing-added-to-commit-but-untracked-files-present/#:~:text=The%20%E2%80%9C%20nothing%20added%20to%20commit%20but%20untracked%20files%20present%20%E2%80%9D%20error,new%20version%20of%20the%20repository.//
+//It worked but then my console sentences stopped working?
+// #-------------------
+// PART 2: Articulating what you are doing
+// #-------------------
+
+// For the following prompts, you will be given a line of code and your task is to type out a Comment,
+// in English, explaining what that line of code is doing, including what the comparison will evaluate to.
+// Be as technically precise as possible, but don't just copy and paste a definition from the readings.
+// Make sure YOU can explain it that way!
+
+console.log(4 < 9);
+// YOU DO: Explain.
+
+var books = 3;
+console.log(4 < books);
+// YOU DO: Explain.
+
+var friends = 6;
+var siblings = 2;
+console.log(friends > siblings);
+// YOU DO: Explain.
+
+var attendees = 9;
+var meals = 8;
+console.log(attendees != meals);
+// YOU DO: Explain.
+
+
+// #-------------------
+// PART 3: Logical Operators
+// #-------------------
+
+var isHungry = true;
+var finishedHomework = false;
+
+// EXAMPLE:
+// Determine if the user is hungry and has completed their homework
+console.log(isHungry && finishedHomework);
+// Determine if the user is hungry or has completed their homework
+console.log(isHungry || finishedHomework);
+
+var lovesToPlay = true;
+var lovesDogPark = false;
+var lovesTreats = true;
+var age = 1;
+
+// YOU DO:
+// Determine if the dog loves to play and loves treats
+
+
+// Determine if the dog loves to play and loves the dog park
+
+
+// Determine if the dog loves to play or loves the dog park
+
+
+// Determine if the dog loves to play and is a puppy
+
+// What did your final line of code evaluate to? Why do you think that is? Explain.
+// ANSWER:
