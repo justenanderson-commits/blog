a73650f2e10b6200140d650777689cb075a8c38e
Justen Anderson <justenanderson@gmail.com>
2022-12-18 18:16:53 -0700

    Add delete functionality.

--- Mod3/practice/blog/src/components/BlogList/BlogList.js
+++ Mod3/practice/blog/src/components/BlogList/BlogList.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
 
-const BlogList = ({ blog }) => {
+const BlogList = ({ blog, handleDelete }) => {
   console.log("Blog: ", blog )
-
 
   return (
     <div className="blog-list">
       <div className="blog-preview" key={ blog.id }>
         <h2>{ blog.title }</h2>
         <p>Written by { blog.author }</p>
+        <button onClick={() => handleDelete(blog.id)}>Delete Blog</button>
       </div>
     </div>
   )
--- Mod3/practice/blog/src/components/Home/Home.js
+++ Mod3/practice/blog/src/components/Home/Home.js
@@ -6,15 +6,20 @@ const Home = () => {
   const [blogs, setBlog] = useState([
     {title: "Title One", body: "Blog Body", author: "Human Person1", id: 1},
     {title: "Title Two", body: "Blog Body", author: "Human Person2", id: 2},
-    {title: "Title Three", body: "Blog Body", author: "Human Person3", id: 3}
+    {title: "Title Three", body: "Blog Body", author: "Human Person1", id: 3}
   ])
 
   const title = "All Blogs!"
+  
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blog => blog.id !== id)
+    setBlog(newBlogs)
+  }
 
   const displayBlogs = blogs.map(blog => {
     return (
       <div className='Home'>
-        <BlogList blog={ blog }/>
+        <BlogList blog={ blog } handleDelete={ handleDelete } />
       </div>
     )
   })
